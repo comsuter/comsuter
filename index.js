084738ffc4ff5da67a8e0d942566d54d9e4f5a67
sue <todayilearn@naver.com>
2024-03-30 15:35:40 +0900

    ul li 태그 추가

--- index.js
+++ index.js
@@ -31,14 +31,18 @@ const parser = new Parser({
     // 피드 목록
     const feed = await parser.parseURL('https://devpad.tistory.com/rss');
 
+    text += `<ul>`;
+
     // 최신글 10개의 글의 제목과 링크를 가져온 후 text에 추가
     for (let i = 0; i < 10; i++) {
         const {title, link} = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
-        text += `<a href=${link} target='_blank'>${title}</a><br />`;
+        text += `<li><a href='${link}' target='_blank'>${title}</a></li><br />`;
     }
+
+    text += `</ul>`;
 
     // README.md 파일 생성
     writeFileSync('README.md', text, 'utf8', (e) => {
